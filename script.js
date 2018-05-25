94c4d5055c7abe123088be17810e111f5b907bc3
Ari Supriatna <arisupriatna@MacBook-Air-Ari.local>
2018-05-25 23:21:53 +0700

    add register

--- script.js
+++ script.js
@@ -8,25 +8,51 @@ document
   .querySelector(".navbar-link-toggle")
   .addEventListener("click", classToggle);
 
-function register(){
-  var at = document.getElementById('email').value;
-  var pass = document.getElementById('pass').value;
-  var confirPass = document.getElementById('pass-repeat').value;
-  var indexOf = at.indexOf('@')
+// function register(){
+//   var at = document.getElementById('email').value;
+//   var pass = document.getElementById('pass').value;
+//   var confirPass = document.getElementById('pass-repeat').value;
+//   var indexOf = at.indexOf('@')
 
-  if(indexOf === -1){
-    alert('email tidak valid')
-    return false
-  } else {
-    return true
-  }
-
-  if(pass !== confirPass){
-    alert('password tidak sama')
-    return false
-  } else {
-    return true
-  }
+//   if(indexOf === -1){
+//     alert('email tidak valid')
+//     return false
+//   } else {
+//     return true
+//   }
   
+// }
+
+function register(form){
+  
+  if(form.email.value === ''){
+    alert('Email tidak boleh kosong!');
+    form.email.focus();
+    return (false)
+  }
+
+  var polaEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if(!polaEmail.test(form.email.value)){
+    alert('Silahkan masukkan email yang valid!');
+    form.email.focus();
+    return false
+  }
+
+  if(form.pass.value === ''){
+    alert('Password tidak boleh kosong!')
+    return (false)
+  }
+
+  if(form.pass.value.length < 6){
+    alert('Panjang password minimal 6 terdiri dari huruf atau angka!');
+    return (false)
+  }
+
+  if(form.pass.value !== form.confirPass.value){
+    alert('Password yang anda masukkan tidak sama!');
+    return (false)
+  }
+
+  return true
 }
 
